e5d0e8ca6ff4a3d796c6b72933fa8e3a8e9f5c42
lambtor <lambtor@yahoo.com>
2023-08-10 12:09:52 -0500

    Add files via upload

--- batteryLED.sh
+++ batteryLED.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #this script will read voltage of battery from beepy
 #convert to a percentage, assuming 3.2v is 0% and 4.2% is maximum
 #set led RGB base on percentage remaining (blue / green / yellow / orange / red)
@@ -45,30 +45,33 @@ set_rgb() {
 		sudo i2cset -y 1 0x1F 0xA2 0x00
 		sudo i2cset -y 1 0x1F 0xA3 0x00
 	fi
-	# turns LED on
+	#turns LED on. use 0xFF for on, 0x00 for off
 	sudo i2cset -y 1 0x1F 0xA0 0xFF
 	#sudo modprobe bbqX0kbd
 }
 
-while true
-do
-	#disable keyboard for battery poll and RGB update
-	sudo modprobe -r bbqX0kbd
-	V=$(i2cget -y 1 0x1F 0x17 w | sed s/0x// | tr '[:lower:]' '[:upper:]')
-	V=$(echo "obase=10; ibase=16; $V" | bc)
-	#using 3.3 as the reference voltage here
-	#tricorder arduino had to use 3.6 as reference mult after a firmware update
-	batt_volt=$(echo "$V * 3.3 * 2 / 4095" | bc -l | cut -c1-5)
+#while true
+#do
+#disable keyboard for battery poll and RGB update
+sudo modprobe -r bbqX0kbd
+V=$(i2cget -y 1 0x1F 0x17 w | sed s/0x// | tr '[:lower:]' '[:upper:]')
+V=$(echo "obase=10; ibase=16; $V" | bc)
+#using 3.3 as the reference voltage here
+#tricorder arduino had to use 3.6 as reference mult after a firmware update
+batt_volt=$(echo "$V * 3.3 * 2 / 4095" | bc -l | cut -c1-5)
 
-	min_volt=3.2
-	max_volt=4.2
+min_volt=3.2
+max_volt=4.2
 
-	percent=$(awk -v min=$min_volt -v max=$max_volt -v now_volt=$batt_volt \
-		'BEGIN { printf "%.1f\n",  {now_volt - min_volt} / {max_volt - min_volt} * 100 }')
+percent=$(awk -v min=$min_volt -v max=$max_volt -v now_volt=$batt_volt \
+	'BEGIN { printf "%.1f\n",  {now_volt - min} / {max - min} * 100 }')
 	
-	set_rgb percent
-	#this re-enables it
-	sudo modprobe bbqX0kbd
-	#poll once per minute maximum
-	sleep 60
-done
+set_rgb percent
+#this re-enables it
+sudo modprobe bbqX0kbd
+#poll once per minute maximum
+#sleep 60
+#done
+
+#to-do: remove while/sleep and just set this up as a cron job?
+# * * * * * /home/batteryLED.sh
