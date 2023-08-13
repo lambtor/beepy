3c48d7011a3cd1e8d5c38a886cb99d2bfcb3304c
lambtor <lambtor@yahoo.com>
2023-08-12 19:27:24 -0500

    Update batteryLED.sh
    remove function, place code inline since should run via cron job

--- batteryLED.sh
+++ batteryLED.sh
@@ -6,11 +6,27 @@
 # V=$(i2cget -y 1 0x1F 0x17 w | sed s/0x// | tr '[:lower:]' '[:upper:]')
 # this re-enables it
 # sudo modprobe bbqX0kbd
-# maybe this should be reconfigured to blink or solid show batt% color when side button pressed/held?
+# maybe this should be reconfigured to blink or solid show batt% color 
+# when side button pressed/held?
 
-set_rgb () {
-	battpct=$1
-	brightness=0.3
+# while true
+# do
+# disable keyboard for battery poll and RGB update
+sudo modprobe -r bbqX0kbd
+V=$(i2cget -y 1 0x1F 0x17 w | sed s/0x// | tr '[:lower:]' '[:upper:]')
+V=$(echo "obase=10; ibase=16; $V" | bc)
+# using 3.3 as the reference voltage here
+# tricorder arduino had to use 3.6 as reference mult after a firmware update
+batt_volt=$(echo "$V * 3.3 * 2 / 4095" | bc -l | cut -c1-5)
+
+min_volt=3.2
+max_volt=4.2
+
+percent=$(awk -v min=$min_volt -v max=$max_volt -v now_volt=$batt_volt \
+	'BEGIN { printf "%.1f\n",  {now_volt - min} / {max - min} * 100 }')
+
+battpct=$1
+brightness=0.3
 	# 0xA1 -> red write, 0xA2 -> green write, 0xA3 -> blue write	
 	# 80 = 128 (255/2)
 	# 55 = 85 (255/3)
@@ -48,25 +64,8 @@ set_rgb () {
 	# turns LED on. use 0xFF for on, 0x00 for off
 	sudo i2cset -y 1 0x1F 0xA0 0xFF
 	# sudo modprobe bbqX0kbd
-}
 
-# while true
-# do
-# disable keyboard for battery poll and RGB update
-sudo modprobe -r bbqX0kbd
-V=$(i2cget -y 1 0x1F 0x17 w | sed s/0x// | tr '[:lower:]' '[:upper:]')
-V=$(echo "obase=10; ibase=16; $V" | bc)
-# using 3.3 as the reference voltage here
-# tricorder arduino had to use 3.6 as reference mult after a firmware update
-batt_volt=$(echo "$V * 3.3 * 2 / 4095" | bc -l | cut -c1-5)
-
-min_volt=3.2
-max_volt=4.2
-
-percent=$(awk -v min=$min_volt -v max=$max_volt -v now_volt=$batt_volt \
-	'BEGIN { printf "%.1f\n",  {now_volt - min} / {max - min} * 100 }')
-	
-set_rgb percent
+# set_rgb percent
 # this re-enables it
 sudo modprobe bbqX0kbd
 # poll once per minute maximum
